c41b7ffa2be27b32a852b150e53dd548412ca725
cstef <colin@petit-suisse.fr>
2021-11-11 11:06:46 +0100

    Update Youtube Regexp

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { Platform } from "./types";
 
 export const REGEXPS: { [platform in Platform]: RegExp } = {
     deezer: /^(?:https?:\/\/|)?(?:www\.)?deezer\.com\/(?:\w{2}\/)?track\/(\d+)/,
-    youtube: /^(?:https?:\/\/|)?(?:www\.)?youtube\.com\/?watch\?v=([a-zA-Z0-9]+)/,
+    youtube: /^(?:https?:\/\/|)?(?:www\.)?youtube\.com\/?watch\?v=([a-zA-Z0-9-_]+)/,
     soundcloud: /^https?:\/\/(soundcloud\.com|snd\.sc)\/([A-Za-z0-9_-]+)\/([A-Za-z0-9_-]+)\/?$/,
     spotify: /https?:\/\/open.spotify.com\/track\/([a-zA-Z0-9]+)|spotify:track:([a-zA-Z0-9]+)/,
 };
